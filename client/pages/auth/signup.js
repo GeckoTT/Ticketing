132c11bf2a3496b1e3aa6a5b94af6b36f4489e63
GeckoTT <GeckoTT@gmail.com>
2021-04-20 11:35:56 +0800

    sign up hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,42 @@
+import { useState } from 'react';
+import axios from 'axios';
+import useRequest from '../../hooks/use-request';
+
 export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+ // const [errors, setErrors] = useState([]);
+  const { doRequest, errors } = useRequest({
+    url: 'api/users/signup',
+    method: 'post',
+    body: {
+      email, password
+    }
+  });
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    //console.log(email, password);
+    doRequest();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div clsssName="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input value={email} 
+          onChange={e => setEmail(e.target.value)} 
+          className="form-control" />
       </div>
       <div clsssName="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input value={password}
+         onChange={e => setPassword(e.target.value)}
+         type="password" className="form-control" />
       </div>
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
